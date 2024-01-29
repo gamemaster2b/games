ea49e424e974c2901f1b5aacc9a530d904f070ce
William <62500337+gamemaster2b@users.noreply.github.com>
2024-01-29 15:17:03 +0200

    Completed 2 player TTT

--- tic_tac_toe/tic_tac_toe_2p.cpp
+++ tic_tac_toe/tic_tac_toe_2p.cpp
@@ -18,7 +18,7 @@ void current_game(char list_current[3][3])
 bool win_test(char p, char l[3][3])
 {
     // This function tests if the player has won
-    if ((l[0][0] == p && l[0][1] == p && l[0][2] == P) || (l[0][0] == p && l[1][0] == p && l[2][0] == P) || (l[0][0] == p && l[1][1] == p && l[2][2] == p) || (l[2][0] == p && l[1][1] == p && l[0][2] == p) || (l[2][0] == p && l[2][1] == p && l[2][2] == p) || (l[1][0] == p && l[1][1] == p && l[1][2] == p) || (l[0][1] == p && l[1][1] == p && l[2][1] == p) || (l[0][2] == p && l[1][2] == p && l[2][2] == p))
+    if ((l[0][0] == p && l[0][1] == p && l[0][2] == p) || (l[0][0] == p && l[1][0] == p && l[2][0] == p) || (l[0][0] == p && l[1][1] == p && l[2][2] == p) || (l[2][0] == p && l[1][1] == p && l[0][2] == p) || (l[2][0] == p && l[2][1] == p && l[2][2] == p) || (l[1][0] == p && l[1][1] == p && l[1][2] == p) || (l[0][1] == p && l[1][1] == p && l[2][1] == p) || (l[0][2] == p && l[1][2] == p && l[2][2] == p))
     {
         return true;
     }
@@ -40,7 +40,7 @@ int main()
     char computer_play_char;
 
     // Here I set the default values for the game
-    char default_value[3][3] = {{'1', '2', '3'}, {'4', '5', '6'}, {'7', '8', '9'}};// This is used as a reference when updating the game in lines 69 and 138
+    char default_value[3][3] = {{'1', '2', '3'}, {'4', '5', '6'}, {'7', '8', '9'}}; // This is used as a reference when updating the game in lines 69 and 138
     char position_value[3][3] = {{A, A, A}, {A, A, A}, {A, A, A}};
 
     // Here I am printing the welcome message and the board positions
@@ -57,8 +57,8 @@ int main()
             current_game(position_value);
             while (valid_play == false)
             {
-                // Here I am making the player play
-                std::cout << "Make your play:";
+                // Here I am making the playerOne play
+                std::cout << "PlayerOne, make your move:";
                 std::cin >> play;
 
                 // Here I am testing if the player play is valid and if it is I am making the play
@@ -83,65 +83,52 @@ int main()
                 }
             }
 
-
             valid_play = false;
             plays_available--;
             std::cout << "Plays available: " << plays_available << std::endl;
 
-            // Here I am making the computer play
+            // Here I am testing if the game is over and if it is I am declaring the winner
+            if (win_test(B, position_value) == true)
+            {
+                std::cout << "🎉Player One won!🎉" << std::endl;
+                round_over = true;
+                break;
+            }
+            else if (win_test(C, position_value) == true)
+            {
+                std::cout << "🎆Player Two Won!🎆" << std::endl;
+                round_over = true;
+                break;
+            }
+            else if (plays_available <= 0)
+            {
+                round_over = true;
+                std::cout << "It's a tie!😑" << std::endl;
+                break;
+            }
+
             while (valid_play == false && plays_available > 0)
             {
-                srand(time(0));
-                computer_play = 1 + (rand() % 9);
-                switch (computer_play)
-                {
-                    // Here I am converting the computer play to a char
-                case 0:
-                    computer_play_char = '7';
-                    break;
-                case 1:
-                    computer_play_char = '1';
-                    break;
-                case 2:
-                    computer_play_char = '2';
-                    break;
-                case 3:
-                    computer_play_char = '3';
-                    break;
-                case 4:
-                    computer_play_char = '4';
-                    break;
-                case 5:
-                    computer_play_char = '5';
-                    break;
-                case 6:
-                    computer_play_char = '6';
-                    break;
-                case 7:
-                    computer_play_char = '7';
-                    break;
-                case 8:
-                    computer_play_char = '8';
-                    break;
-                case 9:
-                    computer_play_char = '9';
-                    break;
-                default:
-                    break;
-                }
-                
-                // Here I am testing if the computer play is valid and if it is I am making the play
+                // Here I am making the playerTwo play
+                std::cout << "PlayerTwo, make your move:";
+                std::cin >> play;
+
+                // Here I am testing if the player play is valid and if it is I am making the play
                 for (int i = 0; i < 3; i++)
                 {
                     for (int j = 0; j < 3; j++)
                     {
-                        if (computer_play_char == default_value[i][j])
+                        if (play == default_value[i][j])
                         {
                             if (position_value[i][j] == A)
                             {
                                 position_value[i][j] = C;
                                 valid_play = true;
                                 current_game(position_value);
+                            }
+                            else
+                            {
+                                std::cout << "Invalid play. Please choose a valid position.";
                             }
                         }
                     }
@@ -154,21 +141,21 @@ int main()
             // Here I am testing if the game is over and if it is I am declaring the winner
             if (win_test(B, position_value) == true)
             {
-                std::cout << "🎉You won!🎉"<<std::endl;
+                std::cout << "🎉Player One Won!🎉" << std::endl;
                 round_over = true;
             }
             else if (win_test(C, position_value) == true)
             {
-                std::cout << "You lost!😢"<<std::endl;
+                std::cout << "🎆Player Two Won!🎆" << std::endl;
                 round_over = true;
             }
             else if (plays_available <= 0)
             {
                 round_over = true;
-                std::cout << "It's a tie!😑"<<std::endl;
+                std::cout << "It's a tie!😑" << std::endl;
             }
         }
-        
+
         // Here I am asking the player if they want to play again
         std::cout << "Do you want to play again? (y/n):";
         std::string play_again;
