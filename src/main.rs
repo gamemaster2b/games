59c4e470865e016cbf8a52d2e208313e6f74776d
William <62500337+gamemaster2b@users.noreply.github.com>
2024-09-14 18:13:58 +0300

    Remove unnecessary files and configurations

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,50 @@
 #![allow(unused)]
 
-use std::{io, ops::Index};
+use std::{
+    collections::btree_map::IterMut,
+    io::{self, Write},
+};
 mod tic_tac_toe;
 
 fn main() {
     println!("Hello, Gamer!");
-    let mut input = Default::default();
+
     'main_loop: loop {
+        let mut input = Default::default();
         let games = ["Tic Tac Toe", "Rock Paper Scissors"];
         println!("This are the games we have: ");
-        for index in 0..games.len() {
-            println!("\t{}. {}", index + 1, games[index]);
+        for (index, item) in games.iter().enumerate() {
+            println!("\t{}. {}", index + 1, item);
         }
         print!("What game would you like to play: ");
+        io::stdout().flush();
         match io::stdin().read_line(&mut input) {
-            Ok(_) => println!("Let's play {:?}.", games[input.trim().parse::<usize>().unwrap_or("bye") - 1 ]  ),
-            Err(_) => println!("What the fuck"),
+            Ok(_) => (),
+            Err(e) => {
+                println!("What the Fuck: {}", e);
+                continue;
+            }
         }
-        if input == "q" {
+
+        println!("{:?} waad", input);
+        if input.trim() == "q" {
             break 'main_loop;
+        }
+        {
+            let input: usize = match input.trim().parse() {
+                Ok(ok) => ok,
+                Err(err) => {
+                    panic!("What the hell: {:?}", err);
+                    continue 'main_loop;
+                    0
+                }
+            };
+
+            match input {
+                1 => tic_tac_toe::tic_tac_toe(),
+                2 => println!("Rock Paper Scissors"),
+                _ => println!("Invalid input"),
+            }
         }
     }
 }
--- src/tic_tac_toe.rs
+++ src/tic_tac_toe.rs
@@ -1,1 +1,87 @@
-    
+use itertools::Itertools;
+use std::{
+    collections::btree_map::IterMut,
+    io::{self, Write},
+};
+
+struct Tile {
+    value: char,
+    x: u8,
+    y: u8,
+}
+
+impl Tile {
+    fn update(&mut self, value: &char) {
+        self.value = *value;
+    }
+}
+impl Default for Tile {
+    fn default() -> Self {
+        Self {
+            value: ' ',
+            x: 0,
+            y: 0,
+        }
+    }
+}
+
+struct Board {
+    size: u8,
+    tiles: Vec<Tile>,
+}
+impl Board {
+    fn new(size: u8) -> Board {
+        let mut tiles: Vec<Tile> = Vec::new();
+        for item in (0..size).cartesian_product(0..size) {
+            tiles.push(Tile {
+                x: item.0,
+                y: item.1,
+                ..Default::default()
+            });
+        }
+        Board { size, tiles }
+    }
+}
+impl Default for Board {
+    fn default() -> Self {
+        let mut d_tiles: Vec<Tile> = Vec::new();
+        for item in (0..3).cartesian_product(0..3) {
+            d_tiles.push(Tile {
+                x: item.0,
+                y: item.1,
+                ..Default::default()
+            });
+        }
+        Self {
+            size: 3,
+            tiles: d_tiles,
+        }
+    }
+}
+fn print(out: &str) {
+    print!("{:?}", out);
+    io::stdout().flush();
+}
+
+pub fn tic_tac_toe() {
+    print("Would you like to vs the computer(c) or a friend(f): ");
+    let mut input = Default::default();
+    match io::stdin().read_line(&mut input) {
+        Ok(_) => (),
+        Err(e) => println!("That is not a valid input: {}", e),
+    }
+    print("What size board would you like to play on: ");
+    let mut board_size = Default::default();
+    match io::stdin().read_line(&mut board_size) {
+        Ok(_) => (),
+        Err(e) => println!("That is not a valid input: {}", e),
+    }
+    let board_size: u8 = match board_size.trim().parse() {
+        Ok(ok) => ok,
+        Err(err) => {
+            println!("That is not a valid input: {:?}", err);
+            return;
+        }
+    };
+    let board = Board::new(board_size);
+}
