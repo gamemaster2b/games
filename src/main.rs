224ed8f1eb4e8163ba164fdc12f21668f4bf98ed
William <62500337+gamemaster2b@users.noreply.github.com>
2024-10-11 12:41:07 +0300

    Restrict paddle position to the table

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ fn main() {
             }),
             ..Default::default()
         }))
-        .add_systems(Startup, (spawn_camera, spawn_players))
+        .add_systems(Startup, (spawn_camera, spawn_players_on_table))
         .add_systems(Update, move_puddles)
         .run();
 }
@@ -33,19 +33,26 @@ struct Puddle {
 
 const PUDDLE_X: f32 = 10.;
 const PUDDLE_Y: f32 = 150.;
-fn spawn_players(mut commands: Commands) {
+
+const TABLE_X: f32 = 700.;
+const TABLE_Y: f32 = 500.;
+fn spawn_players_on_table(mut commands: Commands) {
     commands.spawn(SpriteBundle {
         transform: Transform::from_translation(Vec3::new(0.0, 0.0, -1.0)),
         sprite: Sprite {
             color: colors::BOARD,
-            custom_size: Some(Vec2::new(700., 500.)),
+            custom_size: Some(Vec2::new(TABLE_X, TABLE_Y)),
             ..Default::default()
         },
         ..Default::default()
     });
     commands.spawn((
         SpriteBundle {
-            transform: Transform::from_translation(Vec3::new(-300.0, 0.0, 0.0)),
+            transform: Transform::from_translation(Vec3::new(
+                -((TABLE_X / 2.) - (PUDDLE_X * 2.)),
+                0.0,
+                0.0,
+            )),
             sprite: Sprite {
                 color: colors::TILE_PLACEHOLDER,
                 custom_size: Some(Vec2::new(PUDDLE_X, PUDDLE_Y)),
@@ -60,7 +67,11 @@ fn spawn_players(mut commands: Commands) {
     ));
     commands.spawn((
         SpriteBundle {
-            transform: Transform::from_translation(Vec3::new(300.0, 0.0, 0.0)),
+            transform: Transform::from_translation(Vec3::new(
+                ((TABLE_X / 2.) - (PUDDLE_X * 2.)),
+                0.0,
+                0.0,
+            )),
             sprite: Sprite {
                 color: colors::TILE_PLACEHOLDER,
                 custom_size: Some(Vec2::new(PUDDLE_X, PUDDLE_Y)),
@@ -78,13 +89,22 @@ fn spawn_players(mut commands: Commands) {
 fn move_puddles(
     mut paddles: Query<(&mut Transform, &Puddle), With<Puddle>>,
     input: Res<ButtonInput<KeyCode>>,
+    time: Res<Time>,
 ) {
-    for (mut pos, settings) in paddles {
+    for (mut pos, settings) in &mut paddles {
         if input.pressed(settings.move_up) {
-            pos.translation.y += 1.;
+            pos.translation.y += 1. * time.delta_seconds() * 100.;
+            pos.translation.y = pos.translation.y.clamp(
+                -TABLE_Y / 2. + (PUDDLE_Y / 2.),
+                (TABLE_Y / 2.) - (PUDDLE_Y / 2.),
+            );
         }
         if input.pressed(settings.move_down) {
-            pos.translation.y += -1.;
+            pos.translation.y += -1. * time.delta_seconds() * 100.;
+            pos.translation.y = pos.translation.y.clamp(
+                -TABLE_Y / 2. + (PUDDLE_Y / 2.),
+                (TABLE_Y / 2.) - (PUDDLE_Y / 2.),
+            );
         }
     }
 }
