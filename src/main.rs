6e552d6c76eb6c9a82b617344f5237e5fd4a6371
William <62500337+gamemaster2b@users.noreply.github.com>
2024-10-11 06:26:24 +0300

    ZeroToPong

--- src/main.rs
+++ src/main.rs
@@ -3,59 +3,18 @@
 mod colors;
 
 use std::string::ToString;
-use bevy::prelude::*;
-use bevy::window::WindowMode;
+use bevy::{prelude::*,window::WindowMode};
+
 // use std::{ collections::btree_map::IterMut, io::{self, Write}, };
 // mod tic_tac_toe;
 
 fn main() {
-    /*
-    println!("Hello, Gamer!");
-
-    'main_loop: loop {
-        let mut input = Default::default();
-        let games = ["Tic Tac Toe", "Rock Paper Scissors"];
-        println!("These are the games we have: ");
-        for (index, item) in games.iter().enumerate() {
-            println!("\t{}. {}", index + 1, item);
-        }
-        print!("What game would you like to play: ");
-        io::stdout().flush();
-        match io::stdin().read_line(&mut input) {
-            Ok(_) => (),
-            Err(e) => {
-                println!("What the Fuck: {}", e);
-                continue;
-            }
-        }
-
-        println!("{:?} What the fuck", input);
-        if input.trim() == "q" {
-            break 'main_loop;
-        }
-        {
-            let input: usize = match input.trim().parse() {
-                Ok(ok) => ok,
-                Err(err) => {
-                    panic!("What the hell: {:?}", err);
-                    continue 'main_loop;
-                    0
-                }
-            };
-
-            match input {
-                1 => println!("tic_tact_toe goes here"),//tic_tac_toe::tic_tac_toe(),
-                2 => println!("Rock Paper Scissors"),
-                _ => println!("Invalid input"),
-            }
-        }
-    }
-    */
+    
     App::new()
         .insert_resource(ClearColor(colors::CLEAR_COLOR))
         .add_plugins(DefaultPlugins.set(WindowPlugin {
             primary_window: Some(Window {
-                title: "games".to_string(),
+                title: "ZeroToPong".to_string(),
                 mode: WindowMode::BorderlessFullscreen,
                 ..Default::default()
             }),
@@ -69,14 +28,3 @@ pub fn spawn_camera (mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
 }
 
-const GAMES: [&str; 2] = ["Tic Tac Toe", "Rock Paper Scissors"];
-pub fn main_menu (mut commands: Commands) {
-    commands.spawn(SpriteBundle {
-        sprite: Sprite {
-            custom_size: Some(Vec2::new(720.0, 720.0)),
-            color: colors::BOARD,
-            ..Default::default()
-        },
-        ..Default::default()
-    });
-}
