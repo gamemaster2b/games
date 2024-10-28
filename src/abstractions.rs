d990f3a459dc6fada016386c0a566249602fcd46
William <62500337+gamemaster2b@users.noreply.github.com>
2024-10-28 18:52:05 +0300

    Crate abstractions: a file to store long series of code that can be a function

---
+++ src/abstractions.rs
@@ -0,0 +1,20 @@
+use rand::random;
+
+pub fn get_random_direction(cone: f32) -> f32 {
+    let mut direction: f32 = random::<f32>() * 360.;
+    'set_cone: loop {
+        if direction > (cone / 2.) && direction < 90.
+            || direction > 180. + (cone / 2.) && direction < 270.
+        {
+            direction -= 45.;
+        } else if direction > 270. && direction < 360. - (cone / 2.)
+            || direction > 90. && direction < 180. - (cone / 2.)
+        {
+            direction += 45.;
+        } else {
+            direction = direction.to_radians();
+            break 'set_cone;
+        }
+    }
+    direction
+}
